a13de4516b0f415d4e044d821c78a633baeda101
Belka <belkacem759@gmail.com>
2019-04-13 18:33:33 +0200

    fix addPrice page

--- screens/Addprice.js
+++ screens/Addprice.js
@@ -1,231 +1,122 @@
-import React from "react";
-import { Button, StyleSheet, View, Text, ImageBackground } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
-//import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import React from 'react';
+import { TouchableOpacity, 
+  StyleSheet, 
+  View, Text, 
+  ImageBackground,
+  Image
+} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+
 
 export default class Addprice extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { price: 0 };
+    this.state = {price: '0'};
   }
-  IncrementItem = nb => {
-    this.setState({ price: this.state.price + 1 });
-  };
+  IncrementItem = (nb) => {
+    if(this.state.price.length < 9) {
+    if(this.state.price == '0') {
+      if(nb != 0) 
+      this.setState({ price:'' + nb });
+     }
+     else this.setState({ price: this.state.price + nb });
+    }
+  }
   DecreaseItem = () => {
-    this.setState({ price: this.state.price - 1 });
-  };
-  render() {
-    return (
-      <ImageBackground
-        source={require("../assets/backgrounds/add_price.png")}
-        style={styles.imgbck}
-      >
-        <View style={{ flex: 1 }}>
-          <View style={styles.view1}>
-            <Ionicons
-              name="ios-arrow-back"
-              size={50}
-              color="white"
-              onPress={() => alert("back")}
-            />
-          </View>
-          <View
-            style={{
-              justifyContent: "space-between",
-              alignItems: "stretch",
-              height: "25%",
-              backgroundColor: "skyblue",
-              marginLeft: "13%",
-              marginRight: "13%"
-            }}
-          >
-            <View
-              style={{
-                justifyContent: "center",
-                alignItems: "center",
-                backgroundColor: "red"
-              }}
-            >
-              <Text style={{ fontSize: 25, color: "#fff" }}>
-                Ajoutez votre prix
-              </Text>
-              <View
-                style={{
-                  justifyContent: "center",
-                  alignItems: "center",
-                  backgroundColor: "white",
-                  borderRadius: 30,
-                  elevation: 4,
-                  height: 60,
-                  width: "100%"
-                }}
-              >
-                <Text style={{ fontSize: 30, color: "#ff9933" }}>
-                  {this.state.price}.00 DA
-                </Text>
+    if(this.state.price.length == 1){
+      this.setState({ price:'0'  });
+    }
+    else {
+      this.setState({ price: this.state.price.slice(0, this.state.price.length-1) });
+    }
+    
+  }
+    render() {
+      return (
+        <ImageBackground source={require('../assets/backgrounds/add_price.png')} style={styles.imgbck}>
+         <View style={{flex:1}}>
+            <View style={styles.view1}>
+              <Ionicons name="ios-arrow-back" size={50} color="white" onPress={() => alert('back')}/>
+            </View>
+            <View style={{justifyContent: 'space-between', alignItems: 'stretch', height:hp('25%'),  marginLeft:'13%', marginRight:'13%'}}>
+              <View style={{justifyContent: 'center', alignItems: 'center'}}>
+                <Text style={{fontSize:25, color:'#fff'}}>Ajoutez votre prix</Text>
+                <View style={{justifyContent:'center', alignItems:'center', backgroundColor:'white', borderRadius:30, elevation: 4, height: 60, width:'100%'}}>
+                  <Text style={{fontSize:30,color:'#ff9933'}}>{this.state.price}.00 DA</Text>
+                </View>
+              </View>
+            </View>
+            <View style={{height:'55%', marginLeft:'13%',marginRight:'13%',justifyContent:'space-between', alignItems:'stretch'}}>
+              <View style={{flex: 1,flexDirection: 'row',justifyContent: 'space-between'}}>
+                  <TouchableOpacity onPress={() => this.IncrementItem(1)}>
+                    <Image source={require('../assets/price/1.png')} style={styles.number}/>
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={() => this.IncrementItem(2)}>
+                    <Image source={require('../assets/price/2.png')} style={styles.number}/>
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={() => this.IncrementItem(3)}>
+                    <Image source={require('../assets/price/3.png')} style={styles.number}/>
+                    </TouchableOpacity>   
+              </View>
+              <View style={{flex: 1,flexDirection: 'row',justifyContent: 'space-between'}}>
+                  <TouchableOpacity onPress={() => this.IncrementItem(4)}>
+                    <Image source={require('../assets/price/4.png')} style={styles.number}/>
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={() => this.IncrementItem(5)}>
+                    <Image source={require('../assets/price/5.png')} style={styles.number}/>
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={() => this.IncrementItem(6)}>
+                    <Image source={require('../assets/price/6.png')} style={styles.number}/>
+                    </TouchableOpacity>   
+              </View>
+              <View style={{flex: 1,flexDirection: 'row',justifyContent: 'space-between'}}>
+                  <TouchableOpacity onPress={() => this.IncrementItem(7)}>
+                    <Image source={require('../assets/price/7.png')} style={styles.number}/>
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={() => this.IncrementItem(8)}>
+                    <Image source={require('../assets/price/8.png')} style={styles.number}/>
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={() => this.IncrementItem(9)}>
+                    <Image source={require('../assets/price/9.png')} style={styles.number}/>
+                    </TouchableOpacity>   
+              </View>
+              <View style={{flex: 1,flexDirection: 'row',justifyContent: 'space-between'}}>
+                  <TouchableOpacity onPress={() => this.DecreaseItem()}
+                  onLongPress={()=> this.setState({ price:'0'})}
+                  >
+                    <Image source={require('../assets/price/back.png')} style={styles.number}/>
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={() => this.IncrementItem(0)}>
+                    <Image source={require('../assets/price/0.png')} style={styles.number}/>
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={() => alert(this.state.price)} disabled={this.state.price == 0}>
+                    <Image source={require('../assets/price/ok.png')} style={styles.number}/>
+                    </TouchableOpacity>   
               </View>
             </View>
           </View>
-          <View
-            style={{
-              backgroundColor: "green",
-              height: "55%",
-              marginLeft: "13%",
-              marginRight: "13%",
-              justifyContent: "space-between",
-              alignItems: "stretch"
-            }}
-          >
-            <View
-              style={{
-                flex: 1,
-                flexDirection: "row",
-                justifyContent: "space-between"
-              }}
-            >
-              <View
-                style={{
-                  width: "25%",
-                  height: "80%",
-                  borderRadius: 200,
-                  backgroundColor: "powderblue"
-                }}
-              />
-              <View
-                style={{
-                  width: "25%",
-                  height: "80%",
-                  borderRadius: 200,
-                  backgroundColor: "skyblue"
-                }}
-              />
-              <View
-                style={{
-                  width: "25%",
-                  height: "80%",
-                  borderRadius: 200,
-                  backgroundColor: "steelblue"
-                }}
-              />
-            </View>
-            <View
-              style={{
-                flex: 1,
-                flexDirection: "row",
-                justifyContent: "space-between"
-              }}
-            >
-              <View
-                style={{
-                  width: "25%",
-                  height: "80%",
-                  borderRadius: 200,
-                  backgroundColor: "powderblue"
-                }}
-              />
-              <View
-                style={{
-                  width: "25%",
-                  height: "80%",
-                  borderRadius: 200,
-                  backgroundColor: "skyblue"
-                }}
-              />
-              <View
-                style={{
-                  width: "25%",
-                  height: "80%",
-                  borderRadius: 200,
-                  backgroundColor: "steelblue"
-                }}
-              />
-            </View>
-            <View
-              style={{
-                flex: 1,
-                flexDirection: "row",
-                justifyContent: "space-between"
-              }}
-            >
-              <View
-                style={{
-                  width: "25%",
-                  height: "80%",
-                  borderRadius: 200,
-                  backgroundColor: "powderblue"
-                }}
-              />
-              <View
-                style={{
-                  width: "25%",
-                  height: "80%",
-                  borderRadius: 200,
-                  backgroundColor: "skyblue"
-                }}
-              />
-              <View
-                style={{
-                  width: "25%",
-                  height: "80%",
-                  borderRadius: 200,
-                  backgroundColor: "steelblue"
-                }}
-              />
-            </View>
-            <View
-              style={{
-                flex: 1,
-                flexDirection: "row",
-                justifyContent: "space-between"
-              }}
-            >
-              <View
-                style={{
-                  width: "25%",
-                  height: "80%",
-                  borderRadius: 200,
-                  backgroundColor: "powderblue"
-                }}
-              />
-              <View
-                style={{
-                  width: "25%",
-                  height: "80%",
-                  borderRadius: 200,
-                  backgroundColor: "skyblue"
-                }}
-              />
-              <View
-                style={{
-                  width: "25%",
-                  height: "80%",
-                  borderRadius: 200,
-                  backgroundColor: "steelblue"
-                }}
-              />
-            </View>
-          </View>
-
-          {/* <Button title='++' onPress={this.IncrementItem} />
-            <Button title='--' onPress={this.DecreaseItem} /> */}
-        </View>
-      </ImageBackground>
-    );
+        </ImageBackground>
+      )
+    }
   }
-}
-const styles = StyleSheet.create({
-  imgbck: {
-    flex: 1,
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "stretch"
-  },
-  view1: {
-    height: "12%",
-    width: "10%",
-    marginLeft: "4%",
-    alignItems: "center",
-    justifyContent: "flex-end",
-    backgroundColor: "purple"
-  }
-});
+  const styles = StyleSheet.create({
+    imgbck: {
+      flex: 1,
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'stretch',
+    },
+    view1:{
+      height: '12%', 
+      width:'10%', 
+      marginLeft:'4%',
+      alignItems: 'center', 
+      justifyContent:'flex-end',
+    },
+    number: {
+      resizeMode: 'cover',
+      width: wp(22), 
+      height: hp(15)
+    }
+  })
