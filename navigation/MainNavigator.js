7a5c5b044ba2ebdf428b8aff12156b859a94eaac
Belka <belkacem759@gmail.com>
2019-04-19 17:54:46 +0200

    a

--- navigation/MainNavigator.js
+++ navigation/MainNavigator.js
@@ -24,7 +24,7 @@ const AppStack = createStackNavigator(
         priceAdded: priceAdded
     },
     {
-        initialRouteName : 'nameSearch',
+        initialRouteName : 'Search',
         headerMode: 'none'
     }
     );
